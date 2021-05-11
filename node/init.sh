1803e7c978ecd39a7eddc832d2156de3deb2a41f
StepanTita <stepan.tytarenko@nure.ua>
2021-05-11 17:29:03 +0300

    add node

---
+++ node/init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+rm -rf ~/.band
+
+bandd init node --chain-id odin-testnet
+
+cp /genesis.json ~/.band/config/genesis.json
+
+sed -i 's/enable = false/enable = true/' ~/.band/config/app.toml
+
+# uncomment to disable cors
+#sed -i 's/enabled-unsafe-cors = false/enabled-unsafe-cors = true/' ~/.band/config/app.toml
+#sed -i 's/cors_allowed_origins = \[\]/cors_allowed_origins = ["*"]/' ~/.band/config/config.toml
